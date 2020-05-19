1c00d346006df1ca0f249fc0ad08a120e696e76e
Rumen Roger <rumenroger@gmail.com>
2020-05-19 22:07:20 +0100

    FishTest#00 setup PASS

---
+++ start-point/specs/fish_spec.rb
@@ -0,0 +1,17 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../bear')
+require_relative('../river')
+require_relative('../fish')
+
+class FishTest < MiniTest::Test
+
+    def setup
+        @fish1 = Fish.new("Salmon")
+    end
+
+    
+
+end
