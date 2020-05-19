4e3958ad6a0c2456f4db966a131bd1df21240a52
Rumen Roger <rumenroger@gmail.com>
2020-05-19 21:48:50 +0100

    test#00. bear_spec setup

---
+++ start-point/bear.rb
@@ -0,0 +1,10 @@
+class Bear
+
+    attr_reader :name
+
+    def initialize(bear_name)
+        @bear_name = bear_name
+        @hungry = []
+    end
+
+end
---
+++ start-point/specs/bear_spec.rb
@@ -0,0 +1,17 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../bear')
+require_relative('../river')
+require_relative('../fish')
+
+class BearTest < MiniTest::Test
+
+    def setup
+        @bear = Bear.new("Bob")
+    end
+
+
+
+end
